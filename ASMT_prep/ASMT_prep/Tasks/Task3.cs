3386721ad24ae0ac5d9b1506459bc6d4f904261c
Zsolt Fulop <Zsolt_Fulop@epam.com>
2022-08-01 22:05:09 +0200

    Task3

--- ASMT_prep/ASMT_prep/Tasks/Task3.cs
+++ ASMT_prep/ASMT_prep/Tasks/Task3.cs
@@ -1,22 +1,43 @@
-using System;
+﻿using System;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
 using System.Collections.Generic;
-
+using System.Drawing;
 
 /// <summary>
 ///  Task3 - Implement an image cache using WeakReference; files should be loaded from the disk
 /// </summary>
-namespace dev
+///WeakReference - Ha a hívott oldalon megszűnik a referencea (ahonnan az Add-ot hívtuk), a GC-nek engedi a memóriából felszabadítani az adatot. 
+
+namespace ASMT_prep
 {
-    //class Program
-    //{
-    //    static void Main(string[] args)
-    //    {
-    //        Console.WriteLine();
-    //    }
-    //}
+    public class PictureManager
+    {
+        List<WeakReference<Image>> pictures;
+
+        public void Add(Image image)
+        {
+            if (image == null)
+                return;
+
+            pictures.Add(new WeakReference<Image>(image));
+        }
+
+        public IEnumerable<Image> GetAll()
+        {
+            var deletedItems = new List<WeakReference<Image>>();
+            foreach (var weakImage in pictures)
+            {
+                if (weakImage.TryGetTarget(out Image image))
+                    yield return image;
+                //Időközben megszűnhetett a strong reference -> GC felszabadította -> nincs adat a hivatkozott memória terület.
+                else
+                    deletedItems.Add(weakImage);
+            }
+
+            foreach (var deletedImage in deletedItems)
+                pictures.Remove(deletedImage);
+        }
+    }
 }
-
-
