b816f6fdc46bad8f864a0b0df0a2ffd0c5caf9b5
Zsolt Fulop <Zsolt_Fulop@epam.com>
2022-08-04 17:01:24 +0200

    Task8

--- ASMT_prep/ASMT_prep/Tasks/Task8.cs
+++ ASMT_prep/ASMT_prep/Tasks/Task8.cs
@@ -1,22 +1,72 @@
 using System;
 using System.Collections;
+using System.Collections.Generic;
 
 /// <summary>
 ///  Task8 - Create your own collection
 /// </summary>
 
-//public class MyCollection<T> : IEnumerable<T>
-//{
-//    public IEnumerator<T> GetEnumerator()
-//    {
-//        throw new NotImplementedException();
-//    }
+public class MyCollection<T> : IEnumerable<T>
+{
+    private readonly IEnumerable<T> _sourceData;
 
-//    IEnumerator IEnumerable.GetEnumerator()
-//    {
-//        throw new NotImplementedException();
-//    }
-//}
+    public MyCollection(IEnumerable<T> sourceData)
+    {
+        _sourceData = sourceData;
+    }
+
+    //with yield, there is IEnumerator under the hood
+    //public IEnumerator<T> GetEnumerator()
+    //{
+    //    foreach (var item in _sourceData)
+    //        yield return item;
+    //}
+
+    public IEnumerator<T> GetEnumerator()
+    {
+        return new MyEnumeratorT<T>(_sourceData);
+    }
+
+    //Legacy, dont need to implement
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+        return GetEnumerator();
+    }
+}
+
+public class MyEnumeratorT<T> : IEnumerator<T>
+{   
+    private readonly IEnumerator<T> _sourceEnumerator;
+
+    public MyEnumeratorT(IEnumerable<T> sourceData)
+    {
+        _sourceEnumerator = sourceData.GetEnumerator();        
+    }
+
+    public T Current { get; private set; }
+
+    object IEnumerator.Current => _sourceEnumerator;
+
+    public void Dispose()
+    {
+        _sourceEnumerator.Dispose();
+    }
+
+    public bool MoveNext()
+    {
+        while (_sourceEnumerator.MoveNext())
+        {
+            Current = _sourceEnumerator.Current;
+            return true;
+        }
+        return false;
+    }
+
+    public void Reset()
+    {
+        throw new NotImplementedException();
+    }
+}
 
 //class Program
 //{
